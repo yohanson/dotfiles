d12f90482747c80634c4858b589e0d8a9d89ec2e
Michael Mikhailov <yohanson@ngs.ru>
2018-02-02 12:43:24 +0700

    screenshot.sh: use only import (with xrectsel), add usage

--- bin/screenshot.sh
+++ bin/screenshot.sh
@@ -1,24 +1,61 @@
 #!/bin/bash
 dir=$HOME/Pictures/Screenshots
-
-(
-flock -n 9 || exit 1
-
-filename=$(date +%F-%H%M%S).png
-
-if [ $# -eq 1 ] && [ $1 = 'current' ]; then
-    import -window $(xdotool getwindowfocus) $dir/$filename
-    echo -e "Shot window:\n$(xdotool getwindowfocus getwindowname)" | osd
-elif [ $# -eq 1 ] && [ $1 = 'select' ]; then
-    sleep 0.5
-    echo "Select a region." | osd
-    scrot -e "echo $urlbase/\$n | xsel" -d1 -s $dir'/%Y-%m-%d-%H%M%S.png' && echo "Screenshot saved" | osd
-else
-	import -window root $dir/$filename && echo "Screenshot saved" | osd
+if [ "$1" == "--help" ]; then
+    echo "Usage: $0 [mode]"
+    echo "mode can be one of:"
+    echo "  current - capture current window"
+    echo "  select  - select rectangle to capture"
+    echo "  screen  - capture full screen"
+    exit
 fi
 
-beep -f 400 -l 50
+die()
+{
+    echo "$1"
+    exit 1
+}
 
-sleep 2
+timeout()
+{
+    seconds="$1"
+    message="$2"
+    echo "$message" | osd || exit
+    while [ $seconds -gt 0 ]; do
+        echo "$seconds..."
+        sleep 1
+        let seconds--
+    done | osd
+}
+
+(
+flock -n 9 || die "Already locked"
+
+filename=$(date +%F-%H%M%S).png
+success=true
+
+case "$1" in
+current)
+    import -window $(xdotool getwindowfocus) $dir/$filename
+    echo -e "Shot window:\n$(xdotool getwindowfocus getwindowname)" | osd
+    ;;
+select)
+    echo "Select a region." | osd
+    crop=$(xrectsel) || die "xrectsel error"
+    [ "$crop" != "" ] || die "xrectsel empty result"
+    crop=$(echo $crop | awk -F: '{print $1 "x" $2 "+" $3 "+" $4}')
+    timeout 3 "Capturing in..."
+
+
+    import -window root -crop "$crop" $dir/$filename && echo "Screenshot saved" | osd
+    ;;
+screen)
+	import -window root $dir/$filename && echo "Screenshot saved" | osd
+    ;;
+*)
+    success=false
+    ;;
+esac
+
+$success && sleep 1
 ) 9>/tmp/screenshot.lock
 
